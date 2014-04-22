e71f209b24bb4ac89eef767dcf96af1aaa79d9c5
Ray Henry <ray.henry@alum.mit.edu>
2014-04-22 17:31:40 -0400

    Added mock data capability for links

--- about/about.js
+++ about/about.js
@@ -5,13 +5,19 @@
     var app = angular.module('app');
     
     app.controller('aboutController', ['datacontext', '$scope', function (datacontext, $scope) {
-        var vm = this;
+        var vm = this,
+            promise = datacontext.getLinks();
+        
         vm.title = 'About';
         vm.text = 'This is the about page.';
-        datacontext.getLinks()
+        vm.visibilityStyle = "display:none";
+        promise
             .done(function (data) {
                 vm.links = data;
-                $scope.$apply();
+                vm.visibilityStyle = "display:block";
+                if (!promise.isMock) {
+                    $scope.$apply();
+                }
             })
             .fail(function (jqXHR, textStatus, errorThrown) {
                 vm.error = "Error fetching links: " + errorThrown + " (" + textStatus + ") " + jqXHR.responseText;
--- data/links.js
+++ data/links.js
@@ -15,13 +15,36 @@
                     });
             return promise;
         },
+            
+            mockLinks = function () {
+                var promise = { };
+                promise.isMock = true;
+                promise.done = function (f) {
+                    var data = {};
+                    data.groups = [{}];
+                    data.groups[0].name = "G1";
+                    data.groups[0].links = [{},{}];
+                    data.groups[0].links[0].name = "Test";
+                    data.groups[0].links[0].url = "xxxxx";
+                    data.groups[0].links[1].name = "Test 2";
+                    data.groups[0].links[1].url = "xzzzzzx";
+        
+                    f(data);
+                    return this;
+                };
+                promise.fail = function (f) {
+                    return this;
+                }
+                return promise;
+            },
         
             service = {
-                getLinks: getLinks
+                getLinks: getLinks,
+                mockLinks: mockLinks
             };
 
         return service;
     }
-    app.factory(serviceId, datacontext);
+    app.factory(serviceId, ['$q', datacontext]);
  
 }());
